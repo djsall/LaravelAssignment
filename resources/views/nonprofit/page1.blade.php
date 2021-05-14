cc5de8d4734f82b3ca8fefb6c484efafcc8b67bd
DjSall <proksalevente@gmail.com>
2021-05-14 17:01:21 +0200

    page1 demo

--- resources/views/nonprofit/page1.blade.php
+++ resources/views/nonprofit/page1.blade.php
@@ -1,6 +1,67 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-1
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center bg-warning">
+                    <h1 class="mb-3 header-title">Heading</h1>
+                    <h4 class="mb-3">Subheading</h4>
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at delectus quasi repellendus vero? Cumque dolore odio quasi soluta voluptatem? Aperiam architecto excepturi ipsam optio quae reprehenderit
+                        repudiandae,
+                        sed suscipit?
+                    </div>
+                    <div>A aliquid aspernatur beatae dolore dolorum esse est, exercitationem fuga inventore magnam, maiores mollitia necessitatibus obcaecati odit provident quas quia quisquam rerum sint suscipit tempora vel, velit veniam
+                        vitae
+                        voluptate!
+                    </div>
+                    <div>Accusantium ad assumenda corporis cupiditate ea enim excepturi facere fugiat inventore labore molestias pariatur perferendis praesentium, quam quidem quis quos recusandae repellat repudiandae, sequi sint vero, vitae
+                        voluptate. Amet, explicabo.
+                    </div>
+                    <div>A accusantium ad atque blanditiis culpa cumque doloremque dolores eaque fuga ipsa iste modi molestiae, molestias necessitatibus nemo nostrum officia officiis pariatur praesentium quidem quisquam quod repellendus
+                        voluptates!
+                        Excepturi, expedita!
+                    </div>
+                    <div>Enim eveniet illum mollitia non suscipit! Cupiditate dolores iure maxime qui voluptatibus? Consequuntur cum eius illo ipsum non quam ratione sapiente, sed suscipit, tempore, ullam vel! Consequuntur debitis fugiat
+                        quasi!
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-center pt-5">
+            <div class="col-4 bg-secondary">
+                <h1>First</h1>
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae distinctio enim ex expedita illo in itaque libero nam nisi odio pariatur placeat provident qui repellat repellendus, reprehenderit sint veniam! Autem.
+                </div>
+                <div>Autem, cupiditate earum exercitationem fuga, laudantium maiores officiis quidem quos rerum sint sunt vitae? Doloremque ea error esse ex impedit in, nesciunt nobis odio odit perferendis perspiciatis provident vel
+                    voluptates?
+                </div>
+                <div>Dignissimos magni natus omnis pariatur! Corporis cupiditate dolor eius laboriosam laborum nihil pariatur quam voluptate! Adipisci laborum soluta voluptatum. Deserunt fugiat ipsam iusto reprehenderit! Consequuntur
+                    eveniet molestiae quos sapiente totam.
+                </div>
+            </div>
+            <div class="col-4 bg-success">
+                <h1>Second</h1>
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi atque consectetur consequatur facilis nostrum quos tenetur! Assumenda expedita, fugiat nam possimus provident quibusdam quo sequi! Atque error eveniet
+                    voluptas.
+                </div>
+                <div>Error, nesciunt, optio. Ad aliquam, blanditiis dignissimos dolore doloremque dolores ea enim eos est excepturi illo ipsam ipsum itaque laboriosam quam quod sapiente sit tempora unde velit veritatis vero voluptatem.
+                </div>
+                <div>Accusamus beatae commodi, cum fugit ipsa iure minus nulla odio quos repudiandae. Aperiam consequuntur corporis, id impedit libero magni molestias necessitatibus non optio perspiciatis possimus quos, saepe similique.
+                    Cum, quidem!
+                </div>
+            </div>
+            <div class="col-4 bg-info">
+                <h1>Third</h1>
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, delectus deleniti dolorem error eum ipsa nam porro reiciendis saepe tempora! Ad aliquid animi dignissimos ipsam iusto, provident saepe similique
+                    vel.
+                </div>
+                <div>A dolore est eum maxime quos tenetur ut voluptatem! Accusamus delectus deserunt fugit, labore minus nam natus perferendis quis repudiandae vero. Aliquam aspernatur dolor eaque eligendi harum repellendus voluptate
+                    voluptatibus.
+                </div>
+                <div>Architecto, asperiores at consectetur culpa, cupiditate doloribus ea eius error est exercitationem fugit hic illum impedit minus nisi nulla officiis quod tempora veniam voluptatum. Animi error itaque officiis quia
+                    rerum.
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
