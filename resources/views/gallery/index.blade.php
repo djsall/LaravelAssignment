85670944004b73fa8caa020ca1887ed805979f67
DjSall <proksalevente@gmail.com>
2021-05-14 15:41:57 +0200

    removed redundant libs

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -1,11 +1,5 @@
 @extends('layouts.app')
 
-@section('style')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" integrity="sha512-uURl+ZXMBrF4AwGaWmEetzrd+J5/8NRkWAvJx5sbPSSuOb0bZLqf+tOzniObO00BjHa/dD7gub9oCGMLPQHtQA==" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css" integrity="sha512-nNlU0WK2QfKsuEmdcTwkeh+lhGs6uyOxuUs+n+0oXSYDok5qy0EI0lt01ZynHq6+p/tbgpZ7P+yUb+r71wqdXg=="
-          crossorigin="anonymous"/>
-@endsection
-
 @section('content')
     <div class="container">
         <h1>Image Gallery</h1>
