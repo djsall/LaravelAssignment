805e8ebca49d3776e503998e2d5cf938a1eb55ce
DjSall <proksalevente@gmail.com>
2021-05-14 23:06:16 +0200

    minor tweak in strings

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,30 +8,38 @@
                     <div class="card-header">{{ __('Debug info / Welcome') }}</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+                        <div class="row">
+                            <div class="col-12">
+                                @if(Auth::user())
+                                    Welcome {{ Auth::user()->name }}!
+                                    @if(Auth::user()->isAdmin())
+                                        <p>You are logged in as an administrator account.</p>
+                                    @else
+                                        <p>You are logged in with a non-administrator account.</p>
+                                    @endif
+                                @else
+                                    <p>
+                                        Welcome Guest!
+                                    </p>
+                                    <p>
+                                        Explore our pages using the navbar.
+                                    </p>
+                                    <p>
+                                        Log in, or Sign up to use our member features.
+                                    </p>
+                                @endif
                             </div>
-                        @endif
-                        @if(Auth::user())
-                            Welcome {{ Auth::user()->name }}!
-                            @if(Auth::user()->isAdmin())
-                                <p>You are logged in as an administrator account.</p>
-                            @else
-                                <p>You are logged in with a non-administrator account.</p>
-                            @endif
-                        @else
-                            <p>
-                                Welcome Guest!
-                            </p>
-                            <p>
-                                Explore our pages using the navbar.
-                            </p>
-                            <p>
-                                Log in, or Sign up to use our member features.
-                            </p>
-                        @endif
-                        <a href="https://www.nonprofit.hu/">The page I have chosen to recreate.</a>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <a href="https://www.nonprofit.hu/"><p>The page I have chosen to recreate.</p></a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <a href="https://github.com/djsall/LaravelAssignment"><p>Link to the GitHub repository of the project.</p></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
